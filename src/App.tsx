5058ceff16f1d558b872004374406f2719daf5ac
Ricardo Emerson <ricardo_emerson@yahoo.com.br>
2021-06-01 20:00:49 -0300

    Finalizado o componente Content.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import { useEffect, useState } from 'react';
 
-import { Button } from './components/Button';
-import { MovieCard } from './components/MovieCard';
-
-// import { SideBar } from './components/SideBar';
-// import { Content } from './components/Content';
+import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
 
 import { api } from './services/api';
 
@@ -12,7 +9,6 @@ import './styles/global.scss';
 
 import './styles/sidebar.scss';
 import './styles/content.scss';
-import { SideBar } from './components/SideBar';
 
 export interface GenreResponseProps {
   id: number;
@@ -64,19 +60,7 @@ export function App() {
         setSelectedGenreId={ setSelectedGenreId }
       />
 
-      <div className="container">
-        <header>
-          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-        </header>
-
-        <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-            ))}
-          </div>
-        </main>
-      </div>
+      <Content selectedGenre={ selectedGenre } movies={ movies } />
     </div>
   )
 }
